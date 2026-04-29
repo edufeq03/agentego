5d3b2baf5c0c63e6905062e78496c046efa638c4
edufeq03 <edufeq03@gmail.com>
2026-04-29 01:42:32 -0300

    Expansao de intencoes

--- app/intents.py
+++ app/intents.py
@@ -1,13 +1,17 @@
 from app.openai_client import perguntar
+from app.responses import RESPOSTAS
 
 def classificar_intencao(mensagem):
+    lista_intencoes = ", ".join(RESPOSTAS.keys())
+    
     prompt = f"""
-    Classifique a intenção:
+    Sua tarefa é classificar a intenção da mensagem do usuário.
+    
+    Lista de intenções válidas: [{lista_intencoes}, OUTRO]
 
-    "{mensagem}"
+    Mensagem do usuário: "{mensagem}"
 
-    Responda apenas:
-    HORARIO, PRECO ou OUTRO
+    Responda APENAS com o nome da intenção (exatamente como está na lista em maiúsculo). Se não se encaixar em nenhuma, responda OUTRO.
     """
     
     return perguntar(prompt).strip().upper()
